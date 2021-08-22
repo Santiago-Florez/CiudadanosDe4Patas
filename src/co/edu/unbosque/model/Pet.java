0c71c8ae776bd353c3c17a8c9d39694097251ad1
Santiago-Florez <florezsantiago50@gmail.com>
2021-08-22 17:12:26 -0500

    Atributos Pet

--- src/co/edu/unbosque/model/Pet.java
+++ src/co/edu/unbosque/model/Pet.java
@@ -1,5 +1,16 @@
 package co.edu.unbosque.model;
 
 public class Pet {
-
+	
+	private String id;
+	private long microchip;
+	private String species;
+	private String sex;
+	private String size;
+	private boolean potentDangerous;
+	private String neighborhood;
+	
+	public Pet() {
+		
+	}
 }
