f49225882fe04183f6cf01262385b59d0e2152d8
Santiago-Florez <florezsantiago50@gmail.com>
2021-08-22 17:55:14 -0500

    DAO

---
+++ src/co/edu/unbosque/model/Manager.java
@@ -0,0 +1,16 @@
+package co.edu.unbosque.model;
+
+import java.util.ArrayList;
+
+public class Manager {
+	
+	private ArrayList<Pet> pet;
+	
+	public Manager() {
+		this.pet = new ArrayList<>();
+	}
+	
+	public void assingID() {
+		
+	}
+}
