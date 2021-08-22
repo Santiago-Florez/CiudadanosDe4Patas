309398e136620b6e5be68cb449eba52344420a67
Santiago-Florez <florezsantiago50@gmail.com>
2021-08-22 17:52:12 -0500

    DTO y DAO

--- src/co/edu/unbosque/model/Pet.java
+++ src/co/edu/unbosque/model/Pet.java
@@ -1,7 +1,9 @@
 package co.edu.unbosque.model;
 
-public class Pet {
-	
+import java.io.Serializable;
+
+public class Pet implements Serializable {
+
 	private String id;
 	private long microchip;
 	private String species;
@@ -9,8 +11,76 @@ public class Pet {
 	private String size;
 	private boolean potentDangerous;
 	private String neighborhood;
-	
-	public Pet() {
-		
+
+	public Pet(String id, long microchip, String species, String sex, String size, boolean dangerous,
+			String neighborhood) {
+		this.id = id;
+		this.microchip = microchip;
+		this.species = species;
+		this.sex = sex;
+		this.size = size;
+		this.potentDangerous = dangerous;
+		this.neighborhood = neighborhood;
+	}
+
+	@Override
+	public String toString() {
+		return id + "-" + microchip + species + sex + size + potentDangerous + neighborhood;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public long getMicrochip() {
+		return microchip;
+	}
+
+	public void setMicrochip(long microchip) {
+		this.microchip = microchip;
+	}
+
+	public String getSpecies() {
+		return species;
+	}
+
+	public void setSpecies(String species) {
+		this.species = species;
+	}
+
+	public String getSex() {
+		return sex;
+	}
+
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+
+	public String getSize() {
+		return size;
+	}
+
+	public void setSize(String size) {
+		this.size = size;
+	}
+
+	public boolean isPotentDangerous() {
+		return potentDangerous;
+	}
+
+	public void setPotentDangerous(boolean potentDangerous) {
+		this.potentDangerous = potentDangerous;
+	}
+
+	public String getNeighborhood() {
+		return neighborhood;
+	}
+
+	public void setNeighborhood(String neighborhood) {
+		this.neighborhood = neighborhood;
 	}
 }
